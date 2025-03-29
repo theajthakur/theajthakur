19d86b2fc48bde1ad62cf666de5f8f51504cb616
Vijay Singh <thakurvijayofficial@gmail.com>
2025-03-29 12:39:59 +0530

    Saving Mode in Localstorage

--- src/Context/ModeContext.jsx
+++ src/Context/ModeContext.jsx
@@ -1,12 +1,16 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const ModeContext = createContext();
 
 export const ModeProvider = ({ children }) => {
-  const [mode, setMode] = useState("light");
+  const currentMode = localStorage.getItem("web_light_mode");
+  const [mode, setMode] = useState(currentMode === "light" ? "light" : "dark");
   const toggleMode = () => {
     setMode((prevMode) => (prevMode === "light" ? "night" : "light"));
   };
+  useEffect(() => {
+    localStorage.setItem("web_light_mode", mode);
+  }, [mode]);
 
   return (
     <ModeContext.Provider value={{ mode, toggleMode }}>

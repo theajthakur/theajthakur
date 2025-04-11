ae74a79002944d189424ce695cf7c8f04e0253aa
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-11 09:15:31 +0530

    Added diary project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,53 +4,12 @@ import "./css/projects.css";
 export default function Projects() {
   const projects = [
     {
-      title: "ResQ",
-      thumbnail: "https://picsum.photos/300/200?random=1",
+      title: "Notely",
+      thumbnail: "/assets/images/project/diary.jpeg",
+      accessUrl: "https://theajthakur.github.io/notely",
       description:
-        "A disaster management app that allows users to report disasters, view real-time weather updates, and visualize danger zones on a map.",
-      languages: ["React", "Node.js", "MongoDB", "WeatherAPI"],
-    },
-    {
-      title: "Urban Escape",
-      thumbnail: "https://picsum.photos/300/200?random=2",
-      description:
-        "A game website where users complete missions, capture images, and redeem badges, with admin-controlled features and interactive maps.",
-      languages: ["MERN Stack", "Google Maps API"],
-    },
-    {
-      title: "College Social Media App",
-      thumbnail: "https://picsum.photos/300/200?random=3",
-      description:
-        "A social media app for college students to post updates, follow each other, and interact within a private community.",
-      languages: ["MERN Stack"],
-    },
-    {
-      title: "E-commerce Website",
-      thumbnail: "https://picsum.photos/300/200?random=4",
-      description:
-        "An online store for fashionable and trending clothes with an emphasis on quality, comfort, and durability.",
-      languages: ["Next.js", "MongoDB", "Stripe API"],
-    },
-    {
-      title: "Holi Party Registration Page",
-      thumbnail: "https://picsum.photos/300/200?random=5",
-      description:
-        "A web page for Holi Party event registration, featuring online payments and QR code-based entry validation.",
-      languages: ["Express.js", "EJS", "MongoDB", "JWT"],
-    },
-    {
-      title: "Internship Assignment (Pha5e Hero Section)",
-      thumbnail: "https://picsum.photos/300/200?random=6",
-      description:
-        "A recreation of the Pha5e website's hero section with animations, hover interactions, and mouse movement effects.",
-      languages: ["HTML", "CSS", "JavaScript", "GSAP"],
-    },
-    {
-      title: "GPS Path Tracker",
-      thumbnail: "https://picsum.photos/300/200?random=7",
-      description:
-        "A web app to track users' paths accurately using GPS and Mapbox, with automatic path correction for accuracy.",
-      languages: ["JavaScript", "Mapbox API"],
+        "Notely is a fully client-side diary and to-do app built with ReactJS, focused on user privacy. It lets users write private diary entries, manage tasks by date, mark them as completed or delete them. It features a smart password system that re-authenticates after 1 minute of inactivity, with an option to save the password. Users can export and import backups locally, ensuring complete control over their data.",
+      languages: ["ReactJS", "JavaScript", "LocalStorage"],
     },
   ];
 
@@ -63,7 +22,13 @@ export default function Projects() {
       <div className="projects-cards">
         <div className="row">
           {projects.map((project, index) => (
-            <div className="col-sm-6 col-md-4 col-lg-3 mb-5" key={index}>
+            <div
+              className="col-sm-6 col-md-4 col-lg-3 mb-5"
+              key={index}
+              onClick={() => {
+                window.open(project.accessUrl);
+              }}
+            >
               <div className="project-container">
                 <div className="project-thumbnail">
                   <img src={project.thumbnail} />

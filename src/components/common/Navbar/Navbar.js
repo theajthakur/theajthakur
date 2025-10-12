a97de634dcccd9ba9ff1778e6acd45a8e186d410
VIJAY THAKUR <vijaysingh.handler@gmail.com>
2025-10-12 14:26:24 +0530

    Init: Portfolio App

---
+++ src/components/common/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <div>Home</div>
+        <div>Skills</div>
+        <div>About</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/providers/LayoutProvider.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../common/Navbar/Navbar";
+
+export default function LayoutProvider({ children }) {
+  return (
+    <div className="body-container">
+      <div className="navbar">{/* <Navbar /> */}</div>
+      <div className="main-body">{children}</div>
+    </div>
+  );
+}
